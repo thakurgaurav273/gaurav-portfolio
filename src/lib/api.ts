e8d2e767201ced9789722768676f521c2b0959f3
Gaurav Singh <thakurgaurav273@gmail.com>
2026-06-07 02:01:18 +0530

    change

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api', // Will use vite proxy or just be careful about ports if running separately
+  baseURL: import.meta.env.VITE_API_URL || '/api',
 });
 
 
